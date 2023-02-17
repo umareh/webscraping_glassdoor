e0a71fdc12be71526add7d27075eb46019fda8fc
umareh <umareh@gmail.com>
2023-02-17 09:19:43 -0500

    added link segmentation

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #created by Om Umare
 #Glassdoor parser
 #things to do -
-#   automate input html
+#   automate input html - done
 #   automate max page number
 #   able to search different companies and job titles
 #   create a UI
@@ -13,26 +13,36 @@ from bs4 import BeautifulSoup #import bs4 for html data parsing and managment
 import requests # to be able to make html requests
 from random import randint
 from time import sleep
-textContent = []
-for x in range(1,2): #goes thorugh the first 10 pages - NEED to finda way to see how many pages are there
-    page_link="https://www.glassdoor.com/Interview/Fast-Enterprises-Implementation-Consultant-Interview-Questions-EI_IE241404.0,16_KO17,42_IP"+str(x)+".htm?filter.jobTitleFTS=Implementation+Consultant&filter.offerAccepted=true&filter.offerDeclined=true"
-    page_response = requests.get(page_link,timeout=5)
-    page_content = BeautifulSoup(page_response.content, "html.parser")
-    paragraphs = page_content.ul.find_next_siblings("ul")
-    print(paragraphs)
-#     paragraphs = page_content.find_all("span", {"class": "d-inline-block mb-sm"})
-#     for i in range(0,len(paragraphs)):
-#         textContent.append(paragraphs[i].text)
-#     sleep(randint(2,8))
-# d= Document()
-# for x in range(0,len(textContent)):
-#     p=d.add_paragraph(textContent[x], style='List Bullet')
-#     # list_number(d,p,level=0,num=False)
-# d.save("test.docx")
+def glassfoor(link):
+    for x in range(0,len(link)):
+        if link[x:x++3]=="_IP":
+            a=link[:x+3]
+            b=link[x+4:]
+            break
+#     return a,b
+# a,b=glassfoor("https://www.glassdoor.com/Interview/Susquehanna-International-Group-(SIG)-Assistant-Trader-Interview-Questions-EI_IE24446.0,37_KO38,54_IP3.htm?filter.jobTitleFTS=Assistant+Trader")
+# print(a,b)
+    textContent = []
+    for x in range(30,50): #goes thorugh the first 10 pages - NEED to finda way to see how many pages are there
+        page_link=str(a)+str(x)+str(b)
+        print(page_link)
+        page_response = requests.get(page_link,timeout=5)
+        page_content = BeautifulSoup(page_response.content, "html.parser")
+        paragraphs = page_content.ul.find_next_siblings("ul")
+        # print(paragraphs)
+        paragraphs = page_content.find_all("span", {"class": "d-inline-block mb-sm"})
+        for i in range(0,len(paragraphs)):
+            textContent.append(paragraphs[i].text)
+        #sleep(randint(2,8))
+    d= Document()
+    for x in range(0,len(textContent)):
+        p=d.add_paragraph(textContent[x], style='List Bullet')
+        # list_number(d,p,level=0,num=False)
+    d.save("test.docx")
 
-a="https://www.glassdoor.com/Interview/MarketAxess-Software-Developer-Interview-Questions-EI_IE11426.0,11_KO12,30.htm"
-c="https://www.glassdoor.com/Interview/MarketAxess-Software-Developer-Interview-Questions-EI_IE11426.0,11_KO12,30_IP2.htm?filter.jobTitleFTS=Software+Developer"
-b="https://www.glassdoor.com/Interview/Fast-Enterprises-Implementation-Consultant-Interview-Questions-EI_IE241404.0,16_KO17,42_IP1.htm?filter.jobTitleFTS=Implementation+Consultant&filter.offerAccepted=true&filter.offerDeclined=true"
+# a="https://www.glassdoor.com/Interview/MarketAxess-Software-Developer-Interview-Questions-EI_IE11426.0,11_KO12,30.htm"
+# c="https://www.glassdoor.com/Interview/MarketAxess-Software-Developer-Interview-Questions-EI_IE11426.0,11_KO12,30_IP2.htm?filter.jobTitleFTS=Software+Developer"
+# b="https://www.glassdoor.com/Interview/Fast-Enterprises-Implementation-Consultant-Interview-Questions-EI_IE241404.0,16_KO17,42_IP1.htm?filter.jobTitleFTS=Implementation+Consultant&filter.offerAccepted=true&filter.offerDeclined=true"
 
 
 
